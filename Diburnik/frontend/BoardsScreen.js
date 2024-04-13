2115a9a2792847647475870390655501552ed61c
NadavC3 <nadav3395@gmail.com>
2024-04-13 21:01:53 +0300

    Resolved bug causing app to crash

--- Diburnik/frontend/BoardsScreen.js
+++ Diburnik/frontend/BoardsScreen.js
@@ -22,7 +22,6 @@ const BoardsScreen = ({ route }) => {
   const [newBoardImage, setNewBoardImage] = useState('');
   const navigation = useNavigation();
   const [editMode, setEditMode] = useState(false);
-  const [selectedBoards, setSelectedBoards] = useState([]);
   const [loading, setLoading] = useState(true);
   const [isOnline, setIsOnline] = useState(false);
   const [backgroundColor,setBackgroundColor] = useState('');
@@ -370,7 +369,7 @@ const BoardsScreen = ({ route }) => {
           <ActivityIndicator size="large" color="#0000ff" />
         ) : (
           <View style={styles.boardContainer}>
-            {boards.length === 0 && !editMode ? (
+            {boards?.length === 0 && !editMode ? (
               <View style={{ flex: 0.96, 
               alignItems: 'center', 
               justifyContent: 'center' 
@@ -380,7 +379,7 @@ const BoardsScreen = ({ route }) => {
               style={{ width: 200, height: 222}} />
               </View>
             ) : (
-              boards.map((board) => (
+              boards?.map((board) => (
                 <TouchableOpacity
                   key={board._id}
                   style={[
@@ -404,7 +403,7 @@ const BoardsScreen = ({ route }) => {
                     <View style={{ transform: [{ scale: 0.32 }] }}>
                       <TouchableOpacity
                       style={[styles.deletedBoardBtn, { borderWidth: 8 }]}
-                      onPress={() => handleAddProfile()}>
+                      onPress={() => handleDeleteBoards(board._id)}>
                       <Text style={styles.deleteBoardText}>x</Text>
                       </TouchableOpacity>
                       </View>
@@ -515,7 +514,7 @@ const BoardsScreen = ({ route }) => {
             {/*Go Back button*/}
             <View style={commonStyles.bottomLeft}>
               <TouchableOpacity
-              onPress={() => setIsModalVisible(false)}>
+              onPress={() => {setIsModalVisible(false),setNewBoardName(''),setNewBoardImage('')}}>
                 <Text style={commonStyles.buttonsText}>ביטול</Text>
                 <Image
                 source={require('./assets/appImages/goBackBtn.png')}
